6af5822e6ecca0bfb4b9bcc7b1a83e188b246fc2
Aiant5615 <123721380+Aiant5615@users.noreply.github.com>
2026-09-10 19:49:39 +0900

    Time: 4 ms (90.56%), Space: 11 MB (5.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,15 +1,27 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    for (int i = 0; i < numsSize; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[j] == target - nums[i]) {
-                int* result = malloc(sizeof(int)*2);
-                result[0] = i;
-                result[1] = j;
-                *returnSize = 2;
-                return result;
-            }
+    struct hashTable {
+        int key;
+        int value;
+        UT_hash_handle hh;
+    } *hashTable = NULL, *item;
+    
+    for (int i = 0; i< numsSize; i++) {
+        int complement = target - nums[i];
+        HASH_FIND_INT(hashTable, &complement, item);
+        if (item) {
+            int* result = malloc(sizeof(int) * 2);
+            result[0] = item->value;
+            result[1] = i;
+            *returnSize = 2;
+            HASH_CLEAR(hh, hashTable);
+            return result;
         }
+        item = malloc(sizeof(struct hashTable));
+        item->key = nums[i];
+        item->value = i;
+        HASH_ADD_INT(hashTable, key, item);
     }
     *returnSize = 0;
-    return malloc(sizeof(int)*0);
+    HASH_CLEAR(hh, hashTable);
+    return malloc(0);
 }
